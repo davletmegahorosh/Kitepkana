60d4fdad49eca6221ba0dada51c154fc604c6fb1
DavletGitacc <davletazimov142@gmail.com>
2023-04-24 08:13:25 +0600

    beginining

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Users(models.Model):
+    email = models.TextField()
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=16)
     is_admin = models.BooleanField()
