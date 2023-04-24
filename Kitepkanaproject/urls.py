f141a3c3b829a6b0f1b877a4b8bdf1e5b31d6db6
Alihan-Mahmudov <alihanmahmudov711@gmail.com>
2023-04-24 20:05:15 +0600

    dobavlenie

--- Kitepkanaproject/urls.py
+++ Kitepkanaproject/urls.py
@@ -5,7 +5,9 @@ from books import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/catalog/', views.CatalogApiView.as_view()),
-    path('api/v1/catalog/<int:pk>/', views.CatalogDetailApiView.as_view())
+    path('api/v1/catalog/<int:id>/', views.CatalogDetailApiView.as_view()),
+    path('api/v1/author/<int:id>/', views.AuthorApiView.as_view()),
+    path('api/v1/genre/<int:id>/', views.GenreApiView.as_view())
 ]
 
 
