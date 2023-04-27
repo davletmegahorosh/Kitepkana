24a13be17fa453775165081023f7999c4aba4ef8
ossipova <ossipova11@gmail.com>
2023-04-26 22:45:19 -0500

    "Add users registration"

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AuthorizationApiView
+from .views import AuthorizationApiView, RegistrationApiView
 
 urlpatterns = [
-    path('api/v1/users/auth/', AuthorizationApiView.as_view())
+    path('api/v1/users/auth/', AuthorizationApiView.as_view()),
+    path('api/v1/users/reg/', RegistrationApiView.as_view())
 ]
