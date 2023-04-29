2fb40c21c92c080ba20d4ae8fe6be24e88c1e389
kurmanbek16 <91547120+kurmanbek16@users.noreply.github.com>
2023-04-29 18:08:49 +0600

    update

--- books/serializers.py
+++ books/serializers.py
@@ -7,28 +7,26 @@ class BookSerializer(serializers.ModelSerializer):
     genre_name = SerializerMethodField()
     class Meta:
         model = Book
-        fields = 'cover name author description\n' \
-                 ' pages genre genre_name'.split()
+        fields = 'cover name author description pages genre_name'.split()
 
     def get_genre_name(self, book):
         return book.genre_name
 
 class AuthorSerializer(serializers.ModelSerializer):
-    author_count = SerializerMethodField()
+    Books_count = SerializerMethodField()
     class Meta:
         model = Author
-        fields = 'name surname author_count'.split()
+        fields = 'name surname Books_count'.split()
 
-    def get_author_count(self, obj):
+    def get_Books_count(self, obj):
         return obj.books.all().count()
 
 
 class GenresSerializer(serializers.ModelSerializer):
-    genre_count = SerializerMethodField()
-    # genre_count = BookSerializer()
+    book_count = SerializerMethodField()
     class Meta:
         model = Genre
-        fields = 'name genre_count'.split()
+        fields = 'name book_count'.split()
 
-    def get_genre_count(self, obj):
+    def get_book_count(self, obj):
         return obj.books.all().count()
