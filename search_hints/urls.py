1e763e781935be1d4d14a1f66737f7b675789a1d
arlen <arlenkutmanovvv@gmail.com>
2023-05-28 14:49:03 +0300

    фильтрация жанров,названий книг,авторов

--- search_hints/urls.py
+++ search_hints/urls.py
@@ -1,8 +1,12 @@
-from search_hints.views import BookSearchHintsView
+from search_hints.views import BookSearchHintsView, AuthorSearchAPIView, GenreSearchAPIView, TitleSearchAPIView
 from django.urls import path
 
 
 urlpatterns = [
-    path('search_hints/', BookSearchHintsView.as_view(), name='search-hints')
+    path('search_hints/', BookSearchHintsView.as_view(), name='search-hints'),
+    path('author_filter/', AuthorSearchAPIView.as_view(), name='author-filter'),
+    path('genre_filter/', GenreSearchAPIView.as_view(), name='genre-filter'),
+    path('title_filter/', TitleSearchAPIView.as_view(), name='title-filter')
+
 ]
 
--- search_hints/views.py
+++ search_hints/views.py
@@ -2,6 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from kitepkana.books.models import Books
 from kitepkana.books.serializers import BookSerializer
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
 
 class BookSearchHintsView(APIView):
@@ -11,4 +14,53 @@ class BookSearchHintsView(APIView):
 
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
+
+
+class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
+
+class GenreFilter(filters.FilterSet):
+    genres = CharFilterInFilter(field_name='genre__genre_name', lookup_expr='in')
+
+    class Meta:
+        model = Books
+        fields = ['genre']
+
+
+class GenreSearchAPIView(ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = GenreFilter
+
+
+class TitleFilter(filters.FilterSet):
+    titles = CharFilterInFilter(field_name='title', lookup_expr='in')
+
+    class Meta:
+        model = Books
+        fields = ['title']
+
+
+class TitleSearchAPIView(ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TitleFilter
+
+
+class AuthorFilter(filters.FilterSet):
+    authors = CharFilterInFilter(field_name='author__name', lookup_expr='in')
+
+    class Meta:
+        model = Books
+        fields = ['author']
+
+
+class AuthorSearchAPIView(ListAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AuthorFilter
 
